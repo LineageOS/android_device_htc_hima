fc8e0f85b6b322ab62d765436a88008fff549522
Ethan Chen <intervigil@gmail.com>
2018-08-17 00:17:06 -0700

    hima: Use standard WLAN configuration
    Change-Id: If240db81aa5ef1ec5588c1fa6ecb8e5b707aff36

--- device.mk
+++ device.mk
@@ -311,3 +311,7 @@ PRODUCT_PACKAGES += \
     wificond \
     wpa_supplicant \
     wpa_supplicant.conf
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
